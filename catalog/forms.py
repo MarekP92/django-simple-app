8fc006718499d279bfca3d62e26825e2f577a907
marek <marek559@vp.pl>
2020-03-11 18:11:29 +0100

    changes in project, add new templates

--- catalog/forms.py
+++ catalog/forms.py
@@ -6,7 +6,7 @@ from django import forms
 
 
 class RenewBookForm(forms.Form):
-    """Form for a librarian to renew books."""
+
     renewal_date = forms.DateField(
             help_text="Enter a date between now and 4 weeks (default 3).")
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from .models import Book, Author, BookInstance, Genre
 
 
